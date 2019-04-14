28971192610b9e9402c7729f9845c677fa882645
Alejandro MF <admin@alej.dev>
2019-04-14 12:02:59 +0200

    Configuración PWA completa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'a13-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'aula13';
+export class AppComponent implements OnInit {
+  title = 'Aula 15';
+
+  constructor(private swUpdate: SwUpdate) { }
+
+  ngOnInit(): void {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe(() => {
+        window.location.reload();
+      });
+    }
+  }
 }
