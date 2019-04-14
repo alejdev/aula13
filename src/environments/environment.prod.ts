5f41678f68e4d52556ca91a28b4d1b8d248ffffd
Alejandro MF <admin@alej.dev>
2019-04-14 12:47:32 +0200

    firebase enviroment config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDRyQQC5T6eUxwD5YTnooXuo9mYw5y0uPw',
+    authDomain: 'aula13-4987e.firebaseapp.com',
+    databaseURL: 'https://aula13-4987e.firebaseio.com',
+    projectId: 'aula13-4987e',
+    storageBucket: 'aula13-4987e.appspot.com',
+    messagingSenderId: '889281842330'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDRyQQC5T6eUxwD5YTnooXuo9mYw5y0uPw',
+    authDomain: 'aula13-4987e.firebaseapp.com',
+    databaseURL: 'https://aula13-4987e.firebaseio.com',
+    projectId: 'aula13-4987e',
+    storageBucket: 'aula13-4987e.appspot.com',
+    messagingSenderId: '889281842330'
+  }
 };
 
 /*
