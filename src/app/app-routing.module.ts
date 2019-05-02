0eebb747f3a761a1332bbe373f52c338562d404e
Alejandro MF <admin@alej.dev>
2019-05-02 17:07:11 +0200

    Create Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 import { PupilListComponent } from './components/pupil-list/pupil-list.component';
+import { AulaComponent } from './components/aula/aula.component';
+import { SubjectListComponent } from './components/subject-list/subject-list.component';
+import { ConfigurationComponent } from './components/configuration/configuration.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'alumnos', component: PupilListComponent },
-  { path: 'asignaturas', component: PupilListComponent },
-  { path: 'configuracion', component: PupilListComponent }
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'aula', component: AulaComponent,
+    children: [
+      { path: '', redirectTo: 'alumnos', pathMatch: 'full' },
+      { path: 'alumnos', component: PupilListComponent },
+      { path: 'asignaturas', component: SubjectListComponent },
+      { path: 'configuracion', component: ConfigurationComponent }
+    ]
+  },  
+  { path: '', redirectTo: 'aula', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
