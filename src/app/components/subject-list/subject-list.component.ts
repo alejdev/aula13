75b2b7e63e058684f7a69e0d55ae7808d105929f
Alejandro MF <admin@alej.dev>
2019-05-02 17:04:46 +0200

    Create SubjectList Component

---
+++ src/app/components/subject-list/subject-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'a13-subject-list',
+  templateUrl: './subject-list.component.html',
+  styleUrls: ['./subject-list.component.scss']
+})
+export class SubjectListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
