24b9aae53c14790ed4588c8c6ed21747c4ac4f98
Alejandro MF <admin@alej.dev>
2019-05-02 17:05:02 +0200

    Create Aula Component

---
+++ src/app/components/aula/aula.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { SidenavService } from 'src/app/services/sidenav.service';
+import { onMainContentChange } from 'src/app/animations/animations';
+
+@Component({
+  selector: 'a13-aula',
+  templateUrl: './aula.component.html',
+  styleUrls: ['./aula.component.scss'],  
+  animations: [onMainContentChange]
+})
+export class AulaComponent implements OnInit {
+
+  public onSideNavChange: boolean = true;
+
+  constructor(private sidenavService: SidenavService) {
+    this.sidenavService.sideNavState$.subscribe(res => {
+      this.onSideNavChange = res;
+      console.log('this.onSideNavChange', res)
+    });
+  }
+
+  ngOnInit() {
+  }
+
+}
