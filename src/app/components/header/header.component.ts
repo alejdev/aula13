ba5790b0ed26d9c66f75b904f222bada4e84ab0d
Alejandro MF <admin@alej.dev>
2019-05-03 20:28:12 +0200

    Add link on header title

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'a13-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.sass']
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
   title = 'Aula 13';
