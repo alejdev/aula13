247153722b794e6dc1918efd54559ac1e2847b9e
Alejandro MF <admin@alej.dev>
2019-05-05 11:35:17 +0200

    Update studentList Array

--- src/app/services/student.service.ts
+++ src/app/services/student.service.ts
@@ -1,29 +1,155 @@
 import { Injectable } from '@angular/core';
-
-export interface Student {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
+import { Student } from '../interfaces/student';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StudentService {
 
-  private studentList: Student[] = [
-    { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-    { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-    { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-    { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-    { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-    { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-    { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-    { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-    { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-    { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-  ]
+  private studentList: Student[] = [{
+    id: 1,
+    name: 'Alejandro',
+    surname: 'Martín Fernández',
+    avatar: '',
+    type: 'FEMAE',
+    phones: [{
+      name: 'Padre',
+      number: '654789321'
+    }],
+    subjects: [{
+      id: 1,
+      name: 'Chelo'
+    }]
+  }, {
+    id: 2,
+    name: 'Carmen',
+    surname: 'Valiente Blasco',
+    avatar: '',
+    type: 'Particular',
+    phones: [{
+      name: 'Padre',
+      number: '654987631'
+    }, {
+      name: 'Madre',
+      number: '654987631'
+    }],
+    subjects: [{
+      id: 1,
+      name: 'Chelo'
+    }]
+  }, {
+    id: 3,
+    name: 'María',
+    surname: 'Abreu Márquez',
+    avatar: '',
+    type: 'Particular',
+    phones: [{
+      name: 'Madre',
+      number: '654789321'
+    }],
+    subjects: [{
+      id: 1,
+      name: 'Chelo'
+    }]
+  }, {
+    id: 4,
+    name: 'Enrique',
+    surname: 'Romero Romero',
+    avatar: '',
+    type: 'FEMAE',
+    phones: [{
+      name: 'Madre',
+      number: '687532145'
+    }],
+    subjects: [{
+      id: 1,
+      name: 'Chelo'
+    }]
+  }, {
+    id: 5,
+    name: 'Marco',
+    surname: 'Hoya Cordobés',
+    avatar: '',
+    type: 'Particular',
+    phones: [{
+      name: 'Padre',
+      number: '654789321'
+    }],
+    subjects: [{
+      id: 1,
+      name: 'Chelo'
+    }]
+  }, {
+    id: 6,
+    name: 'Raquel',
+    surname: 'Hoyos',
+    avatar: '',
+    type: 'FEMAE',
+    phones: [{
+      name: 'Madre',
+      number: '654789321'
+    }],
+    subjects: [{
+      id: 1,
+      name: 'Chelo'
+    }]
+  }, {
+    id: 7,
+    name: 'Marta',
+    surname: '',
+    avatar: '',
+    type: 'Particular',
+    phones: [{
+      name: 'Padre',
+      number: '654789321'
+    }],
+    subjects: [{
+      id: 1,
+      name: 'Chelo'
+    }]
+  }, {
+    id: 8,
+    name: 'Diego',
+    surname: 'Bravo Rojo',
+    avatar: '',
+    type: 'FEMAE',
+    phones: [{
+      name: 'Padre',
+      number: '654789321'
+    }],
+    subjects: [{
+      id: 1,
+      name: 'Chelo'
+    }]
+  }, {
+    id: 9,
+    name: 'Daniel',
+    surname: 'Rubio Rebollo',
+    avatar: '',
+    type: 'FEMAE',
+    phones: [{
+      name: 'Madre',
+      number: '654789321'
+    }],
+    subjects: [{
+      id: 1,
+      name: 'Chelo'
+    }]
+  }, {
+    id: 10,
+    name: 'Saray',
+    surname: 'Brachiller',
+    avatar: '',
+    type: 'FEMAE',
+    phones: [{
+      name: 'Padre',
+      number: '654789321'
+    }],
+    subjects: [{
+      id: 1,
+      name: 'Chelo'
+    }]
+  }]
 
   constructor() {
   }
