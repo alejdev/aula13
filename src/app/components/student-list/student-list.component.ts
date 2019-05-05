fda1ba2ea16408c4be99b3cbdc49c4a94436af03
Alejandro MF <admin@alej.dev>
2019-05-05 11:36:38 +0200

    Change StudentListTable by Student componenet

--- src/app/components/student-list/student-list.component.ts
+++ src/app/components/student-list/student-list.component.ts
@@ -1,7 +1,6 @@
-import { SelectionModel } from '@angular/cdk/collections';
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { StudentService, Student } from 'src/app/services/student.service';
-import { MatPaginator, MatTableDataSource, MatSort } from '@angular/material';
+import { Component, OnInit } from '@angular/core';
+import { StudentService } from 'src/app/services/student.service';
+import { Student } from 'src/app/interfaces/student';
 
 @Component({
   selector: 'a13-student-list',
@@ -10,52 +9,18 @@ import { MatPaginator, MatTableDataSource, MatSort } from '@angular/material';
 })
 export class StudentListComponent implements OnInit {
 
-  title: string = 'Alumnos'
-  displayedColumns: string[] = ['select', 'position', 'name', 'weight', 'symbol']
-  dataSource = new MatTableDataSource<Student>(this.studentService.getStudentList())
-  selection = new SelectionModel<Student>(true, [])
+  title = 'Alumnos'
+  columns = ['nombre', 'telefono', 'spacer', 'tipo']
+  studentList: Student[]
+  search: string = ''
 
-  @ViewChild(MatPaginator) paginator: MatPaginator
-  @ViewChild(MatSort) sort: MatSort
-
-  constructor(private studentService: StudentService) { }
-
-  ngOnInit() {
-    this.dataSource.paginator = this.paginator
-    this.dataSource.sort = this.sort
+  constructor(private studentService: StudentService) {
+    this.studentList = this.studentService.getStudentList()
   }
 
-  /**
-   * Aplica un filtro a la tabla
-   * @param filterValue filtro
-   */
-  applyFilter(filterValue: string) {
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-  }
+  ngOnInit() { }
 
-  /**
-   * Comprueba si todos los elementos están seleccionados
-   */
-  isAllSelected() {
-    return this.selection.selected.length === this.dataSource.data.length;
-  }
-
-  /**
-   * Selecciona todos o ningún elemento
-   */
-  masterToggle() {
-    this.isAllSelected() ?
-      this.selection.clear() :
-      this.dataSource.data.forEach(row => this.selection.select(row));
-  }
-
-  /** 
-   * Label del checkbox
-   */
-  checkboxLabel(row?: Student): string {
-    if (!row) {
-      return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
-    }
-    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.position + 1}`;
+  sort(value) {
+    console.log(value);
   }
 }
