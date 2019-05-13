7987be194c6aa3fad4540c2a4f0641ec5a73a4f9
Alejandro MF <admin@alej.dev>
2019-05-13 21:07:02 +0200

    Added enviroments to .gitignore

---
+++ src/environments/environment.git.ts
@@ -0,0 +1,15 @@
+
+// Copy this file twice and rename: 'environment.ts' and 'environment.prod.ts'
+
+// Replace this by you firebase config
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "<API_KEY>",
+    authDomain: "<PROJECT_ID>.firebaseapp.com",
+    databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
+    projectId: "<PROJECT_ID>",
+    storageBucket: "<BUCKET>.appspot.com",
+    messagingSenderId: "<SENDER_ID>",
+  }
+};
