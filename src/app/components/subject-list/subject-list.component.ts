5cd3f1520f7e0c7d4a537be91749be8a835d798a
Alejandro MF <admin@alej.dev>
2019-05-27 17:43:16 +0200

    SubjectList title

--- src/app/components/subject-list/subject-list.component.ts
+++ src/app/components/subject-list/subject-list.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubjectListComponent implements OnInit {
 
+  title: string = 'Asignaturas'
+
   constructor() { }
 
   ngOnInit(): void { }
