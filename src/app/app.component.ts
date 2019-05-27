38bba6108ef6c60f3e29e25a5594fe433c60b87e
Alejandro MF <admin@alej.dev>
2019-05-27 17:47:11 +0200

    delete unnecessary app.component template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
+
 import { Component, OnInit, } from '@angular/core';
 import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'a13-root',
-  templateUrl: './app.component.html',
+  template: '<router-outlet></router-outlet>',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
