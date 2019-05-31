cb4f0ea69c389424ab6d1bb086589f5eb8047f99
Alejandro MF <admin@alej.dev>
2019-05-31 21:01:44 +0200

    Logiin confirm password

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit, HostBinding } from '@angular/core'
+import { Component, HostBinding, OnInit } from '@angular/core'
 import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 import { Router } from '@angular/router'
+import { AuthService } from 'src/app/services/auth.service'
 import { UtilService } from 'src/app/services/util.service'
+import { CustomValidator } from 'src/app/validators/custom-validator'
 
 @Component({
   selector: 'a13-login',
@@ -12,27 +14,50 @@ export class LoginComponent implements OnInit {
 
   title: string = 'Aula 13'
   formGroup: FormGroup
+  formValidators: any
   passwordType = 'password'
+  login: boolean = true
   @HostBinding('class') classes = 'light-theme'
 
-  constructor(private formBuilder: FormBuilder, private utilService: UtilService, private router: Router) { }
+  constructor(private formBuilder: FormBuilder, private utilService: UtilService, private router: Router, private authService: AuthService) { }
 
   ngOnInit(): void {
-    this.formGroup = this.formBuilder.group({
-      email: [null, [Validators.required, Validators.pattern(this.utilService.regExp.email)]],
-      password: [null, [Validators.required, Validators.minLength(6)]]
-    })
+    this.formValidators = {
+      email: [null, Validators.compose([Validators.required, Validators.pattern(this.utilService.regExp.email)])],
+      password: [null, Validators.compose([Validators.required, Validators.minLength(6)])],
+    }
+    this.formGroup = this.createSignInForm()
   }
 
   loginWithEmail(): void {
-    // if (this.formGroup.valid) {
-    this.router.navigateByUrl('/aula')
-    // }
+    if (this.formGroup.valid) {
+      this.router.navigate(['aula'])
+    }
+  }
+
+  registerWithEmail(): void {
+    this.authService.register(this.formGroup.value)
   }
 
   loginWithGoogle() { }
   loginWithFacebook() { }
   loginWithTwitter() { }
+
+  toggleSign() {
+    this.login = !this.login
+    this.formGroup = this.login ? this.createSignInForm() : this.createSignUpForm()
+  }
+
+  createSignInForm(): FormGroup {
+    return this.formBuilder.group(this.formValidators)
+  }
+
+  createSignUpForm(): FormGroup {
+    return this.formBuilder.group(
+      { ...this.formValidators, confirmPassword: [null, Validators.compose([Validators.required])] },
+      { validator: CustomValidator.passwordMatchValidator }
+    )
+  }
 
   togglePasswordVisibility() {
     this.passwordType = this.passwordType === 'password' ? 'text' : 'password'
@@ -49,8 +74,26 @@ export class LoginComponent implements OnInit {
         return `ERR.PASSWORD_REQUIRED`
       case type === 'password' && form.hasError('minlength'):
         return `ERR.PASSWORD_INVALID`
+      case type === 'confirmPassword' && form.hasError('required'):
+        return `ERR.CONFIRM_PASSWORD_REQUIRED`
+      case type === 'confirmPassword' && form.hasError('notSame'):
+        return `ERR.CONFIRM_PASSWORD_NOT_MATCH`
       default:
         return ``
+    }
+  }
+
+  getSignOptions() {
+    return {
+      button: {
+        color: this.login ? 'accent' : 'primary',
+        text: this.login ? 'SIGN.IN' : 'SIGN.UP',
+      },
+      footer: {
+        color: this.login ? 'primary' : 'accent',
+        hint: this.login ? 'MSG.NO_ACCOUNT' : 'MSG.YES_ACCOUNT',
+        text: this.login ? 'SIGN.UP' : 'SIGN.IN',
+      }
     }
   }
 }
