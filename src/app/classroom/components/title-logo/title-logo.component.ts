69fe4c08bd985e2c26876926f84bf688e8e2c03c
Alejandro Martín Fernández <aledroner@gmail.com>
2021-03-30 04:27:44 +0200

    Add logo to Header

--- src/app/classroom/components/title-logo/title-logo.component.ts
+++ src/app/classroom/components/title-logo/title-logo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, Input, OnInit } from '@angular/core'
 
 @Component({
   selector: 'a13-title-logo',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./title-logo.component.scss']
 })
 export class TitleLogoComponent implements OnInit {
+
+  @Input() showTitle: boolean
+  @Input() showLogo: boolean
 
   constructor() { }
 
